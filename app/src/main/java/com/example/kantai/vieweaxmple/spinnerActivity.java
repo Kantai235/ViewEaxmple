15ccc6f3cb24f406a7fb4237abdb6d2d303656a4
乾太 <huevo235@gmail.com>
2016-11-04 22:37:23 +0800

    我好像新增了 Spinner
    我好像新增了 Spinner

--- app/src/main/java/com/example/kantai/vieweaxmple/spinnerActivity.java
+++ app/src/main/java/com/example/kantai/vieweaxmple/spinnerActivity.java
@@ -4,13 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class spinnerActivity extends AppCompatActivity {
+
+    protected Spinner spinner;
+    protected SpinnerAdapter spinnerAdapter;
+    protected List<String> adapterData = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinner);
+
+        // 利用 for 迴圈去新增 Adapter 的資料
+        for (int i = 0; i <= 20; i++)
+            this.adapterData.add(String.valueOf(i));
+
+               
+        this.spinner = (Spinner) findViewById(R.id.spinnerActivity_spinner);
+//        this.spinnerAdapter =
     }
 
     @Override
